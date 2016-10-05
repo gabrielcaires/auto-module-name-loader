d330427a8f55780a8cb24554c01e0d009ea5ff02
Gabriel Caires <gcaires@thoughtworks.com>
2016-10-05 15:49:18 -0300

    Ignore modules that do not use module

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const hasModule = (content) => content.indexOf('module') >= 0;
+
 module.exports = function (content) {
+  if (!hasModule(content)) return content;
+
   return `module.id = "${this.resourcePath}"; ${content}`;
 };
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -3,14 +3,21 @@ let expect = require('chai').expect;
 let loader = require('../index');
 
 describe('auto amd id', () => {
-	let moduleWithoutId = 'define("name", ["dependency1", "dependency2"], function(dep1, dep2) {}';
+	let moduleWithoutId = 'define("name", ["dependency1", "dependency2"], function(dep1, dep2) { console.log(module); }';
 	let filename = 'dir1/dir2/dir3/file.js';
+	let context = {resourcePath: filename};
 
-	it('parses files with plugins', () => {
-		let context = {resourcePath: filename};
+	it('adds modules name to files that uses module', () => {
 		let result = loader.call(context, moduleWithoutId);
 		let expected = `module.id = "${filename}"; ${moduleWithoutId}`;
 
 		expect(result).to.be.equal(expected);
+	});
+
+	it('does not add module name if module is not used', () => {
+		let fileWithoutModule = 'define("name", ["dep1"], function(dep1) {};';
+		let result = loader.call(context, fileWithoutModule);
+
+		expect(result).to.be.equal(fileWithoutModule);
 	});
 });
